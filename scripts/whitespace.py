dea30c0a4e6c91231ae94ed19a84dd77d5d61e6c
Jason Kepler <jasonkepler1@gmail.com>
2023-08-04 12:42:24 -0700

    Better script naming

--- scripts/whitespace.py
+++ scripts/whitespace.py
@@ -1,13 +1,13 @@
 import argparse
 
-def convert_csv_to_space_separated(input_file, output_file):
+def control_separator_whitespace(input_file, output_file):
     try:
         with open(input_file, 'r') as tab_file:
-            data = [line.strip().split() for line in tab_file]
+            data = [line.strip().split() for line in tab_file] # remove extra whitespace
         
         with open(output_file, 'w') as space_separated_file:
             for row in data:
-                space_separated_file.write(f"{row[0]} {row[1]}\n")
+                space_separated_file.write(f"{row[0]} {row[1]}\n") # single space-separated
                 
         print(f"Conversion successful. Output saved to {output_file}")
     
@@ -17,9 +17,9 @@ def convert_csv_to_space_separated(input_file, output_file):
         print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Convert a whitespace (ex. tab) separated edge list to single space-separated format.")
+    parser = argparse.ArgumentParser(description="Convert a whitespace-separated edge list (ex. tabs) to single space-separated format.")
     parser.add_argument("input_file", help="Path to the input TXT file")
     parser.add_argument("output_file", help="Path to the output TXT file")
     args = parser.parse_args()
     
-    convert_csv_to_space_separated(args.input_file, args.output_file)
+    control_separator_whitespace(args.input_file, args.output_file)
